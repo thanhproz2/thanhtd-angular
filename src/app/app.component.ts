06f6fd74b1f0af247bdb67cd64d6798ed9ed4e61
Thanh Tran <thanhtd@imt-soft.com>
2020-11-22 08:40:51 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'hkt-app';
+
+  ngOnInit() {
+    this.isMobileMenu();
+  }
+
+  isMobileMenu() {
+    setTimeout(() => {
+      if (window && window.innerWidth > 991) {
+        console.log(window.innerWidth);
+  
+        return false;
+      }
+      return true;
+    }, 1000);
+    
+  }
 }
